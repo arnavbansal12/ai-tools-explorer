0f39f7c5075c5281744505530879a0e3b7e9804b
arnavbansal12 <arnavbansal23feb2007@gmail.com>
2026-03-29 15:43:24 +0530

    added dark theme

--- script.js
+++ script.js
@@ -79,5 +79,29 @@ function applyFilters() {
   displayTools(result);
 }
 
+
 // Start
 fetchTools();
+
+//dark theme
+
+const themeToggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  themeToggle.textContent = "☀️ Light Mode";
+}
+
+// Toggle theme
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    themeToggle.textContent = "☀️ Light Mode";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeToggle.textContent = "🌙 Dark Mode";
+  }
+});
